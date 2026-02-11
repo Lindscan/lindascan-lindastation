4cb4f3b7e3bf7f924a16216eab8c924390288b19
Lindscan <linda.scan@atomicmail.io>
2026-02-11 18:11:20 +0330

    Linda

--- test/setup/LindaWeb.js
+++ test/setup/LindaWeb.js
@@ -1,3 +1,3 @@
-module.exports = require('../../node_modules/lindaweb/dist/LindaWeb.node');
+module.exports = require('../../node_modules/lindaweb/dist/LindaWeb.node.js');
 // module.exports = require('../../../linda-web/dist/LindaWeb.node');
 
